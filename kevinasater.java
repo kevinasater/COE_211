497a96414684e5ac3c6be826e67d5a8839e24690
kevinasater <99675702+kevinasater@users.noreply.github.com>
2022-02-14 20:44:46 +0200

    Update kevinasater.java

--- kevinasater.java
+++ kevinasater.java
@@ -1,2 +1,6 @@
- Why can’t a bike stand on its own? It’s two tired.
- I told my doctor that I broke my arm in two places. He told me to stop going to those places.
+public class kevinasater{
+ public static void main(String[] args){
+System.out.println("Why can’t a bike stand on its own? It’s two tired.");
+System.out.println("I told my doctor that I broke my arm in two places. He told me to stop going to those places.");
+ }
+}
