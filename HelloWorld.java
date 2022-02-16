befafb3241c1f7022a86521147b0fb06c4eef077
kevinasater <99675702+kevinasater@users.noreply.github.com>
2022-02-16 19:39:20 +0200

    Update and rename kevinasater.java to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class kevinasater{
  public static void main(String[] args){
+System.out.println("Hello from GitHub");
 System.out.println("Why can’t a bike stand on its own? It’s two tired.");
 System.out.println("I told my doctor that I broke my arm in two places. He told me to stop going to those places.");
  }
